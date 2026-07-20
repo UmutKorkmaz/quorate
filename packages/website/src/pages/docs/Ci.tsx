46cbaa30ec0715211eefc9f392516d57a4881890
Umut Korkmaz <umutkorkmaz@outlook.com.tr>
2026-07-20 20:35:50 +0300

    chore: pin v1.3.0 action references

--- packages/website/src/pages/docs/Ci.tsx
+++ packages/website/src/pages/docs/Ci.tsx
@@ -211,7 +211,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v4
-      - uses: UmutKorkmaz/quorate@81deb42f04b3b21557b0a055001c48daa4d01baf
+      - uses: UmutKorkmaz/quorate@1e7796b0f86cdbacadf149637c87b9812b246303
         env:
           OPENROUTER_API_KEY: \${{ secrets.OPENROUTER_API_KEY }}
         with:
